13cf7b08be73c412d5143ca4f8973b146dacbfc3
gensakudan <teamgaijinalex@gmail.com>
2023-05-31 07:58:45 +0200

    Replace tabs with four spaces

--- src/assets/tomba.js
+++ src/assets/tomba.js
@@ -1,50 +1,50 @@
 var Tomba = new class {
-	hover = false;
-	shock = false;
-	bbox  = document.getElementById('tomba-gif');
-	e     = this.bbox.childNodes[0];
-	
-	update() {
-		if (this.shock) {
-			this.e.classList.remove('hover');
-			this.e.classList.add('shock');
-			this.e.src = '/assets/img/fm3.gif';
-		} else if (this.hover) {
-			this.e.classList.remove('shock');
-			this.e.classList.add('hover');
-			this.e.src = '/assets/img/fm2.gif';
-		} else {
-			this.e.classList.remove('hover', 'shock');
-			this.e.src = '/assets/img/fm.gif';
-		}
-	}
-	
-	setHover(state) {
-		if (this.hover != state) {
-			this.hover = state;
-			this.update();
-		}
-	}
-	
-	setShock(state) {
-		if (this.shock != state) {
-			this.shock = state;
-			this.update();
-		}
-	}
+    hover = false;
+    shock = false;
+    bbox  = document.getElementById('tomba-gif');
+    e     = this.bbox.childNodes[0];
+    
+    update() {
+        if (this.shock) {
+            this.e.classList.remove('hover');
+            this.e.classList.add('shock');
+            this.e.src = '/assets/img/fm3.gif';
+        } else if (this.hover) {
+            this.e.classList.remove('shock');
+            this.e.classList.add('hover');
+            this.e.src = '/assets/img/fm2.gif';
+        } else {
+            this.e.classList.remove('hover', 'shock');
+            this.e.src = '/assets/img/fm.gif';
+        }
+    }
+    
+    setHover(state) {
+        if (this.hover != state) {
+            this.hover = state;
+            this.update();
+        }
+    }
+    
+    setShock(state) {
+        if (this.shock != state) {
+            this.shock = state;
+            this.update();
+        }
+    }
 }
 
 var mouse = {x: undefined, y: undefined};
 var collide = false;
 
 Tomba.bbox.addEventListener('mouseenter', () => {
-	collide = true;
-	Tomba.setHover(true);
+    collide = true;
+    Tomba.setHover(true);
 });
 Tomba.bbox.addEventListener('mouseleave', () => {
-	collide = false;
-	Tomba.setHover(false);
-	Tomba.setShock(false);
+    collide = false;
+    Tomba.setHover(false);
+    Tomba.setShock(false);
 });
 window.addEventListener('mousedown', (event) => Tomba.setShock(collide && event.button == 0));
 window.addEventListener('mouseup', () => Tomba.setShock(false));
--- src/index.php
+++ src/index.php
@@ -1,50 +1,50 @@
 <?php include_once('assets/messages.php'); ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<link rel="icon" href="/resources/favicon-512x512.png">
-		<link rel="stylesheet" href="/assets/style.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta charset="UTF-8">
-		<title>♥TOMBA CLUB!♥</title>
-	</head>
-	<body>
-		<div class="bar"><bar1><bar2></bar2><bar3></bar3></bar1></div>
-		<div id="tomba-gif"><img src="/assets/img/fm.gif"></div>
-		<img src="/assets/img/TcL.png" width="500" height="500">
-		<p>
-			Welcome to the TOMBA CLUB website on the interwebs!<br><br>
-			Where do you want to go today?
-		</p>
-		<div class="marquee"><p><?= $messages[rand(0, count($messages)-1)] ?></p></div>
-		<menu>
-			<a href="/wiki/Main_Page">
-				<img src="/assets/img/wiki.png" width="460" height="290">
-				Tomba Wiki!
-			</a>
-			<a href="/interviews.html">
-				<img src="/assets/img/interview.png" width="460" height="290">
-				Interviews
-			</a>
-			<a href="/merch.html">
-				<img src="/assets/img/merch.png" width="460" height="290">
-				Merch We Found
-			</a>
-			<!--
-			<a href="#">
-				<img src="/assets/img/faq.png" width="460" height="290">
-				Frequently Asked Questions
-			</a>
-			-->
-		</menu>
-		<img src="/assets/img/uc.gif">
-		<footer>
-			<small>
-				Tomba! &copy; 1997-2000 Whoopee Camp Co. Ltd.<br>
-				&copy; 2023 TOMBA CLUB (Please don’t sue us, Tokuro!)
-			</small>
-		</footer>
-		<script src="/assets/tomba.js"></script>
-		<script src="/assets/marquee.js"></script>
-	</body>
+    <head>
+        <link rel="icon" href="/resources/favicon-512x512.png">
+        <link rel="stylesheet" href="/assets/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <title>♥TOMBA CLUB!♥</title>
+    </head>
+    <body>
+        <div class="bar"><bar1><bar2></bar2><bar3></bar3></bar1></div>
+        <div id="tomba-gif"><img src="/assets/img/fm.gif"></div>
+        <img src="/assets/img/TcL.png" width="500" height="500">
+        <p>
+            Welcome to the TOMBA CLUB website on the interwebs!<br><br>
+            Where do you want to go today?
+        </p>
+        <div class="marquee"><p><?= $messages[rand(0, count($messages)-1)] ?></p></div>
+        <menu>
+            <a href="/wiki/Main_Page">
+                <img src="/assets/img/wiki.png" width="460" height="290">
+                Tomba Wiki!
+            </a>
+            <a href="/interviews.html">
+                <img src="/assets/img/interview.png" width="460" height="290">
+                Interviews
+            </a>
+            <a href="/merch.html">
+                <img src="/assets/img/merch.png" width="460" height="290">
+                Merch We Found
+            </a>
+            <!--
+            <a href="#">
+                <img src="/assets/img/faq.png" width="460" height="290">
+                Frequently Asked Questions
+            </a>
+            -->
+        </menu>
+        <img src="/assets/img/uc.gif">
+        <footer>
+            <small>
+                Tomba! &copy; 1997-2000 Whoopee Camp Co. Ltd.<br>
+                &copy; 2023 TOMBA CLUB (Please don’t sue us, Tokuro!)
+            </small>
+        </footer>
+        <script src="/assets/tomba.js"></script>
+        <script src="/assets/marquee.js"></script>
+    </body>
 </html>

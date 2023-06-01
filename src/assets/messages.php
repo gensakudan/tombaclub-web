30a2fca25cd4774cbe2dad75e5149be3cba410cb
gensakudan <teamgaijinalex@gmail.com>
2023-06-01 21:20:52 +0200

    Add more marquee messages

--- src/assets/messages.php
+++ src/assets/messages.php
@@ -14,6 +14,13 @@ $messages = [
     'Tomba 2 is better than Tomba 1!',
     'Partying like it’s 1997!',
     'Don’t click the Tomba... or do. I’m not your dad.',
-    'Tomba 1 lowest% is currently at 277,100 AP, 61 events found, and 57 events cleared.'
+    'Tomba 1 lowest% is currently at 277,100 AP, 61 events found, and 57 events cleared.',
+    '藤原徳郎先生、どうかお話かけください！',
+    'Is it a sex thing or can you win?',
+    'We glub but glob, come on and join the Tomba Club!',
+    'We need to add that to the TCRF page!',
+    '80 or 81?',
+    'Pog Bag',
+    'Kainen was the 10-quadrillion-year-old man all along!'
 ];
 ?>

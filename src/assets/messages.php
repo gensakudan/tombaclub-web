e437fe1eab90b653698f3cea41eb4518a2158f7c
gensakudan <teamgaijinalex@gmail.com>
2023-06-01 20:31:48 +0200

    Add new marquee messages

--- src/assets/messages.php
+++ src/assets/messages.php
@@ -1,6 +1,9 @@
 <?php
 $messages = [
     'Tomba 1 is better than Tomba 2!',
-    'Tomba 2 is better than Tomba 1!'
+    'Tomba 2 is better than Tomba 1!',
+    'Partying like it’s 1997!',
+    'Don’t click the Tomba... or do. I’m not your dad.',
+    'Tomba 1 lowest% is currently at 277,100 AP, 61 events found, and 57 events cleared.'
 ];
 ?>
